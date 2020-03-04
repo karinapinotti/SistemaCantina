955123f994eb2301ee32445a14a9d4601e2551e9
Karina Bruschi Pinotti <59757343+karinapinotti@users.noreply.github.com>
2020-03-03 22:36:11 -0300

    Dia 1
    Sistema básico para vendas de cantina, ao adicionar um item ele exibe a foto do mesmo "importante que a pasta imagens esteja salva na unidade C", o valor do item é somado ao valor total.
    Pretendo incluir mais funções, como um botão para fechar a compra e iniciar um nova, e também uma função onde o próprio usuário possa definir os produtos e valores dos mesmos.

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace SistemaCantina
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.picImagem = new System.Windows.Forms.PictureBox();
+            this.lstCaixa = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(168, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sistema Cantina";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(71, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CÓDIGO DO PRODUTO:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(291, 342);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "VALOR TOTAL:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(75, 117);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(172, 20);
+            this.txtCodigo.TabIndex = 3;
+            this.txtCodigo.TextChanged += new System.EventHandler(this.txtCodigo_TextChanged);
+            // 
+            // picImagem
+            // 
+            this.picImagem.Location = new System.Drawing.Point(75, 162);
+            this.picImagem.Name = "picImagem";
+            this.picImagem.Size = new System.Drawing.Size(172, 173);
+            this.picImagem.TabIndex = 4;
+            this.picImagem.TabStop = false;
+            // 
+            // lstCaixa
+            // 
+            this.lstCaixa.FormattingEnabled = true;
+            this.lstCaixa.Location = new System.Drawing.Point(295, 97);
+            this.lstCaixa.Name = "lstCaixa";
+            this.lstCaixa.Size = new System.Drawing.Size(246, 238);
+            this.lstCaixa.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 426);
+            this.Controls.Add(this.lstCaixa);
+            this.Controls.Add(this.picImagem);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.PictureBox picImagem;
+        private System.Windows.Forms.ListBox lstCaixa;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaCantina
+{
+    public partial class Form1 : Form
+    {
+        string[] produtos = new string[10];
+        string[] codigo = new string[10];
+        double[] valor = new double[10];
+        double soma;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void label3_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void txtCodigo_TextChanged(object sender, EventArgs e)
+        {
+            if (txtCodigo.Text.Length == 3)
+            {
+                int indice = 0;
+                for (int prod = 1; prod < codigo.Length; prod++)
+                {
+                    if (txtCodigo.Text == codigo[prod])
+                    {
+                        indice = prod;
+                    }
+                }
+                if (indice == 0)
+                {
+                    MessageBox.Show("Produto não encntrado");
+                }
+                else
+                {
+                    lstCaixa.Items.Add(txtCodigo.Text + " -- " + produtos[indice] + "-- R$" + valor[indice]);
+                    soma = soma + valor[indice];
+                    label3.Text = ("Valor Total R$ " + soma);
+                    picImagem.ImageLocation = "c:/imagens/" + codigo[indice] + ".jpeg";
+                    txtCodigo.Text = "";
+                    txtCodigo.Focus();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            carregarArray();
+            soma = 0;
+        }
+
+        private void carregarArray()
+        {
+            codigo[1] = "001";
+            codigo[2] = "002";
+            codigo[3] = "003";
+            codigo[4] = "004";
+            codigo[5] = "005";
+            produtos[1] = "Pastel";
+            produtos[2] = "Coxinha";
+            produtos[3] = "Hot_Dog";
+            produtos[4] = "Chocolate";
+            produtos[5] = "Suco";
+            valor[1] = 6.00;
+            valor[2] = 5.00;
+            valor[3] = 12.00;
+            valor[4] = 3.50;
+            valor[5] = 8.00;
+}
+        private void picImagem_Click(object sender, EventArgs e)
+        {
+        }
+        private void lstCaixa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
